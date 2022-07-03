b0e4ae7c675305aa924c65ef9610b4ea90304a40
Lyon <lyon7500@gmail.com>
2022-07-03 11:01:44 -0700

    Added in update secret functionality

--- src/components/pages/ManageSecrets.js
+++ src/components/pages/ManageSecrets.js
@@ -5,14 +5,15 @@ import {
   changeAccountRole,
 } from "../../service/AdminPanel/AdminPanel.js";
 import { UserContext } from "../../auth/UserProvider.js";
-import { useState, useContext, useEffect } from "react";
+import { useState, useContext, useEffect, useRef } from "react";
 import { Form, Button, Row, Col, Table } from "react-bootstrap";
-import { addSecret, deleteSecret, fetchSecrets } from "../../service/Vault/SecretVault.js";
+import { addSecret, deleteSecret, fetchSecrets, updateSecret } from "../../service/Vault/SecretVault.js";
 
 const ManageSecrets = (props) => {
   const [selectFile, setSelectFile] = useState();
   const [isFilePicked, setIsFilePicked] = useState(false);
   const [inputName, setInputName] = useState("");
+  const inputRef = useRef("");
 	const {user} = useContext(UserContext);
 
   const changeHandler = (event) => {
@@ -20,6 +21,7 @@ const ManageSecrets = (props) => {
     setIsFilePicked(true);
   };
   const [showAdd, setShowAdd] = useState(0);
+  const [showEdit, setShowEdit] = useState(null);
 
   const [data, setData] = useState([]);
 
@@ -51,8 +53,21 @@ const ManageSecrets = (props) => {
         console.log(res)
       })
       
-      data.unshift(submitObject);
-      setShowAdd(0);
+      window.location.reload()
+    }
+  };
+
+  function handleClickSubmitEdit(secret) {
+    if (inputRef.current.value != "") {
+      let submitObject = {
+        value: inputRef.current.value
+      };
+      
+      updateSecret(secret, submitObject, user.jwt).then(res => {
+        console.log(res)
+      })
+
+      window.location.reload()
     }
   };
 
@@ -79,23 +94,36 @@ const ManageSecrets = (props) => {
               <button onClick={handleClick}>+ Add Secret</button>
             </td>
           </tr>
+          <tr>
+            <td>
+              <h5>Secret value</h5>
+            </td>
+            <td>
+              <h5>Secret owned by</h5>
+            </td>
+            <td>
+              <h5>Secret creation date</h5>
+            </td>
+            <td></td>
+            <td></td>
+          </tr>
           {(() => {
             if (showAdd != 0) {
               return (
                 <tr>
-                  <td>Secret: </td>
                   <td>
                     <input
                       type="text"
                       id="message"
                       name="message"
+                      placeholder="Enter Secret Value"
                       onChange={handleChangeName}
                       autoComplete="off"
                     ></input>
                   </td>
                   <td></td>
                   <td></td>
-
+                  <td></td>
                   <td>
                     <button onClick={handleClickSubmit}>*SUBMIT*</button>
                   </td>
@@ -107,37 +135,57 @@ const ManageSecrets = (props) => {
           })()}
 
           {data.map(function (Secret, index) {
-            console.log(Secret.value);
 
             const handleDelete = async (secret, index, e) => {
-
-                await deleteSecret(secret,user.jwt);
-                
+              await deleteSecret(secret,user.jwt);
               setData(data.filter((v, i) => i !== index));
             };
 
             const handleEdit = (index, e) => {
-              setData(data.filter((v, i) => i !== index));
-
-              setShowAdd(1);
+              setShowEdit(index);
             };
 
-            return (
-              <tr>
-                <td>{Secret.value}</td>
-                <td></td>
-                <td></td>
-                <td>
-                  {" "}
-                  <button onClick={(e) => handleEdit(index, e)}>Edit</button>
-                </td>
-                <td>
-                  <button onClick={(e) => handleDelete(Secret.secret, index, e)}>
-                    Delete
-                  </button>
-                </td>
-              </tr>
-            );
+            if (showEdit != index) {
+              return (
+                <tr>
+                  <td>{Secret.value}</td>
+                  <td>{Secret.username}</td>
+                  <td>{Secret.date}</td>
+                  <td>
+                    {" "}
+                    <button onClick={(e) => handleEdit(index, e)}>Share</button>
+                    {" "}
+                    <button onClick={(e) => handleEdit(index, e)}>Edit</button>
+                  </td>
+                  <td>
+                    <button onClick={(e) => handleDelete(Secret.secret, index, e)}>
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              );
+            } else {
+              return (
+                <tr>
+                  <td>
+                    <input
+                      ref={inputRef}
+                      type="text"
+                      id="editmessage"
+                      name="editmessage"
+                      placeholder="Enter Secret Value"
+                      autoComplete="off"
+                    ></input>
+                  </td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td>
+                    <button onClick={() => handleClickSubmitEdit(Secret.secret)}>*SUBMIT*</button>
+                  </td>
+                </tr>
+              );
+            }
           })}
         </thead>
       </Table>
